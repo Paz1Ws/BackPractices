d3fa6a0d4b873e1542a6c85bc4f228a413cae016
Paz1Ws <142619752+Paz1Ws@users.noreply.github.com>
2023-12-14 12:49:27 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿using System.Numerics;
-
-namespace Proyectos_VS
+﻿namespace Proyectos_VS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // 4/12/2023 (Sum of the pairs and odd positions)
             /*
+            // 4/12/2023 (Sum of the pairs and odd positions)
+
                 int sumP = 0; int sumI = 0;
                 Random rdn = new Random();
                 int[] arr = new int[5];
@@ -19,9 +18,10 @@ namespace Proyectos_VS
                     sumI += (i + 1) % 2 == 0 ? arr[i] : 0;
 
                 }
-                Console.WriteLine($"\nSum of odds positions: {sumP}\nSum of pairs positions: {sumI}");*/
+                Console.WriteLine($"\nSum of odds positions: {sumP}\nSum of pairs positions: {sumI}");
+
             // 7/12/2023 (Check if the number value of one phrase is prime)
-            /*
+            
             Dictionary<string, int> WordAndValue = new Dictionary<string, int>();
             Console.Write("Words in Phrase?: ");
             int winp = int.Parse(Console.ReadLine());
@@ -95,10 +95,10 @@ namespace Proyectos_VS
         }
 
             return true;
-            */
+            
             // 9/12/2023 (Encoding Nico Cipher)
 
-            /*
+            
             string message = Console.ReadLine();
             string key = Console.ReadLine();
             string encodedMessage = NicoCipher(message, key);
@@ -168,9 +168,9 @@ namespace Proyectos_VS
                 return order;
             
             }
-            */
-            // 11/12/2023
-            /*
+            
+            // 11/12/2023 (The smallest number divisible between 1 to the user number)
+            
             // Solution 1:
             Console.Write("Number: ");
             int numdiv = int.Parse(Console.ReadLine());
@@ -212,11 +212,11 @@ namespace Proyectos_VS
                 return lcm.ToString();
             }
 
-            */
+            
 
-            // 11/12/2023
+            // 11/12/2023 (Calculate zeros at end of the factorial number of a whole number)
 
-            Console.WriteLine("Number?: ");
+            Console.Write("Number?: ");
             BigInteger n = BigInteger.Parse(Console.ReadLine());
             BigInteger CopyN = n;
             //int fact = n;
@@ -235,7 +235,144 @@ namespace Proyectos_VS
                     break;
             }
             Console.WriteLine($"The number {CopyN} have {CountZeros} at its end.");
+            
+            // LAMBDAS
+            Func<int, double> sd = (si) => si * 0.5;
+            Console.WriteLine(sd(7));
+
+            dynamic nms = new List<int> { 1, 3, 5, 7, 9 };
+            Action<int> print = Console.WriteLine;
+            Action<List<int>> printtwo = (nms) =>
+             {
+                 foreach (int n in nms)
+                     print(n);
+             };
+            printtwo(nms);
+            
+            string f = null;
+            Func<int, Func<int, bool>, string> funcP = (n, funcH) =>
+            {
+                if (funcH(n) == true)
+                {
+                    f = "Maybe odd";
+                    Console.Write(f);
+                }
+                else if (funcH(n) == false)
+                {
+                    f = "Maybe odd and even";
+                    Console.Write(f);
+                }
+                return null;
+            };
+            Console.WriteLine(funcP(177, n => n % 3 == 0 ? true : false));
+
+            Random random = new Random();
+            Action<int> show = Console.Write;
+            int a, b = 0;
+            a = random.Next(1, 5); b = random.Next(1, 5);
+            int[] ints = new int[10];
+
+            for (int i = 0; i < 5; i++)
+            {
+                ints[i] = random.Next(0, 6);
+                show(ints[i]);
+            }
+            Console.WriteLine("\n" + a + " " + b);
+            Func<int, int, int> result = (i1, i2) => ints[a - 1] * ints[b - 1];
+            Console.WriteLine("\n" + result(ints[a - 1], ints[b - 1]));
+
+            
+            // 13/12/2023 - LINQ
+            var listStudents = new List<Student>
+            {
+
+             new Student { Id = 1, Name = "Eduardo", Average = 20},
+             new Student { Id = 2, Name = "Gabriel", Average = 17},
+             new Student { Id = 1, Name = "Jhon", Average = 14},
+             new Student { Id = 2, Name = "Miguel", Average = 9 },
+             new Student { Id = 1, Name = "Blas", Average = 12},
+
+            };
+            var listSubjects = new List<Subjects>
+            {
+            new Subjects {StudentId = 1, CourseName = "History", Grade = 1 },
+            new Subjects {StudentId = 2, CourseName = "Geography", Grade = 2},
+
+            };
+
+            var students = from s in listStudents
+                           join ss in listSubjects on s.Id equals ss.StudentId
+                           where s.Average >= 12
+                           orderby s.Average
+                           select new
+                           {
+                               s.Name,
+                               s.Average,
+                               ss.CourseName
+                               
+                           };
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+            
+            // 14/10/2023 - LINQ Intermediate
+
+            var listStudents = new List<Estudiante>
+            {
+                new Estudiante { Id = 1, Nombre = "Eduardo", CursosInscritos = new List<int> { 1, 2, 4 } },
+                new Estudiante { Id = 2, Nombre = "Gabriel", CursosInscritos = new List<int> { 1, 4, 6 } },
+                new Estudiante { Id = 3, Nombre = "Jhon", CursosInscritos = new List<int> { 3, 5 } }
+            };
+
+            var list2Students = new List<Estudiante>
+            {
+                new Estudiante { Id = 4, Nombre = "Hugo", CursosInscritos = new List<int> { 1, 2 } },
+                new Estudiante { Id = 5, Nombre = "Dina", CursosInscritos = new List<int> { 4, 6 } }
+            };
+
+            var listSubjects = new List<Curso>
+            {
+                new Curso { Id = 1, Nombre = "Moderno", Categoria = "Arte" },
+                new Curso { Id = 2, Nombre = "Bioquímica", Categoria = "Ciencia" },
+                new Curso { Id = 3, Nombre = "Híndu", Categoria = "Filosofía" },
+                new Curso { Id = 2, Nombre = "Cuántica", Categoria = "Ciencia" },
+                new Curso { Id = 1, Nombre = "Renacentista", Categoria = "Arte" }
+            };
+
+            var newList = (from e in listStudents.Union(list2Students)
+                           where e.CursosInscritos.Contains(1) || e.CursosInscritos.Contains(2)
+                           select e)
+                           .Select(e => $"{e.Nombre} inscrito en {string.Join(", ", listSubjects
+                           .Where(s => e.CursosInscritos.Contains(s.Id))
+                           .Select(s => s.Nombre))}, pertenece a la categoría {string.Join(", ", listSubjects
+                           .Where(s => e.CursosInscritos.Contains(s.Id))
+                           .Select(s => s.Categoria))}");
+
+            foreach (var e in newList)
+                Console.WriteLine(e);
+
+            Console.WriteLine($"Hay {newList.Count()} estudiantes inscritos en Arte y/o Ciencia.");
+
+
+
         }
 
+        public class Estudiante
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public List<int> CursosInscritos { get; set; }
+        }
+
+        public class Curso
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Categoria { get; set; }
+        }
+            */
+
+        }
     }
 }
